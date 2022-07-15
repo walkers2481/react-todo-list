56384187a61ed0ef207038ad0b1c256bd8e15756
kobayashi <walkers2481@gmail.com>
2022-07-15 11:45:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const todoListStyle = {
+    backgroundColor: "#afeeee",
+    height: "300px",
+    maxWidth: "600px",
+    margin: "20px auto",
+    borderRadius: "15px",
+  }
+
+  const inputStyle = {
+    border: "none",
+    borderRadius: "5px",
+    display: "block",
+    margin: "0px auto",
+    position: "relative",
+    top: "10px",
+  }
+
+  const ulStyle = {
+    position: "relative",
+    top: "10px",
+    left: "50px",
+  }
+
+  const handleInputChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (todo !== "") {
+      setTodos([...todos, {id: todos.length + 1, text: todo.trim()}]);
+    }
+    setTodo("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      className="App"
+      style={todoListStyle}>
+      <form onSubmit={handleFormSubmit}>
+        <input 
+          name="todo" 
+          type="text" 
+          placeholder="Create a new todo"
+          value={todo}
+          onChange={handleInputChange}
+          style={inputStyle} />
+      </form>
+      <ul 
+        className="todo-list"
+        style={ulStyle}>
+        {todos.map((todo) => (
+            <li key={todo.id}>{todo.text}</li>
+          ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
